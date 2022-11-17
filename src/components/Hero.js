8db8577e47b43142cc3b97638579e604333944ae
Triola Jackson <triolajackson@gmail.com>
2022-11-17 13:15:44 -0600

    Final commit

---
+++ src/components/Hero.js
@@ -0,0 +1,99 @@
+import gaming from "../images/image-gaming-growth.jpg";
+import retroPCs from "../images/image-retro-pcs.jpg";
+import laptops from "../images/image-top-laptops.jpg";
+// import heroImg_desktop from "../images/image-web-3-desktop.jpg";
+import heroImg from "../images/image-web-3-mobile.jpg";
+const Hero = () => {
+  return (
+    <main>
+      <div className="mainContainer">
+        <section className="mainContent">
+          <picture>
+            <source
+              srcset="image-web-3-desktop.jpg"
+              media="(min-width: 1200px)"
+            />
+            <img
+              className="heroImg"
+              src={heroImg}
+              alt="Different color shapes"
+            />
+          </picture>
+          <div className="box">
+            <h1>The Bright Future of Web 3.0?</h1>
+            <div className="mainContent__info">
+              <p>
+                We dive into the next evolution of the web that claims to put
+                the power of the platforms back into the hands of the people.
+                But is it really fulfilling its promise?
+              </p>
+
+              <button href="/more">Read More</button>
+            </div>
+          </div>
+        </section>
+
+        <article>
+          <h2>New</h2>
+          <div className="articles">
+            <div className="article">
+              <h3 className="articleTitle">Hydrogen VS Electric Cars</h3>
+              <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+            </div>
+            <span className="line"></span>
+            <div className="article">
+              <h3 className="articleTitle">The Downsides of AI Artistry</h3>
+              <p>
+                What are the possible adverse effects of on-demand AI image
+                generation?
+              </p>
+            </div>
+            <span className="line"></span>
+            <div className="article">
+              <h3 className="articleTitle">Is VC Funding Drying Up?</h3>
+              <p>
+                Private funding by VC firms is down 50% YOY. We take a look at
+                what that means.
+              </p>
+            </div>
+          </div>
+        </article>
+      </div>
+
+      <section className="featureArticles">
+        <div className="featureContent">
+          <img className="featureImg" src={retroPCs} alt="Retro PCs" />
+          <div className="featureBox">
+            <span className="number">01</span>
+            <h4 className="heading">Reviving Retro PCs</h4>
+            <p className="description">
+              What happens when old PCs are given modern upgrades?
+            </p>
+          </div>
+        </div>
+        <div className="featureContent">
+          <img className="featureImg" src={laptops} alt="Retro PCs" />
+          <div className="featureBox">
+            <span className="number">02</span>
+            <h4 className="heading">Top 10 Laptops of 2022</h4>
+            <p className="description">
+              Our best picks for various needs and budgets.
+            </p>
+          </div>
+        </div>
+        <div className="featureContent">
+          <img className="featureImg" src={gaming} alt="Retro PCs" />
+          <div className="featureBox">
+            <span className="number">03</span>
+            <h4 className="heading">The Growth of Gaming</h4>
+            <p className="description">
+              How the pandemic has sparked fresh opportunities.
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar.js
@@ -0,0 +1,72 @@
+import PropTypes from "prop-types";
+import React, { useState } from "react";
+
+import { ReactComponent as Logo } from "../images/logo.svg";
+
+const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const handleToggle = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
+  return (
+    <header className="header">
+      <Logo className="logo" />
+
+      <nav className="navBar">
+        <span className="hamburger" onClick={handleToggle}>
+          {navbarOpen ? (
+            <svg width="32" height="31" xmlns="http://www.w3.org/2000/svg">
+              <g fill="#00001A" fill-rule="evenodd">
+                <path d="m2.919.297 28.284 28.284-2.122 2.122L.797 2.419z" />
+                <path d="M.797 28.581 29.081.297l2.122 2.122L2.919 30.703z" />
+              </g>
+            </svg>
+          ) : (
+            <svg width="40" height="17" xmlns="http://www.w3.org/2000/svg">
+              <g fill="#00001A" fill-rule="evenodd">
+                <path d="M0 0h40v3H0zM0 7h40v3H0zM0 14h40v3H0z" />
+                <path d="M0 0h40v3H0z" />
+              </g>
+            </svg>
+          )}
+        </span>
+        <div className={`menuBackground ${navbarOpen ? "open" : ""}`}>
+          <div className={`menuNav ${navbarOpen ? "showMenu" : ""}`}>
+            <div className="menuContent">
+              <li className="nav-link">
+                <a href="/home" onClick={() => closeMenu()}>
+                  Home
+                </a>
+              </li>
+              <li className="nav-link">
+                <a href="/new">New</a>
+              </li>
+              <li className="nav-link">
+                <a href="/popular">Popular</a>
+              </li>
+              <li className="nav-link">
+                <a href="/trending">Trending</a>
+              </li>
+              <li className="nav-link">
+                <a href="/categories">Categories</a>
+              </li>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+Navbar.defaultProps = {
+  title: "Test",
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default Navbar;
